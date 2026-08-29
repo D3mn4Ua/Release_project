eb1a05baf6258e52f03ca9cc67add5f231d28a50
D3mn4Ua <davidkolup@gmail.com>
2026-08-29 16:55:57 +0300

    Auth_system fully working

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import register_view
+from .views import *
 
 urlpatterns = [
-    path('registration/', register_view, name='registration')
+    path('', home_view, name='home'),
+    path('registration/', registration_view, name='registration'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout')
 ]
--- auth_system/views.py
+++ auth_system/views.py
@@ -1,16 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from .forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 
-def register_view(request):
-    if request.method == "POST":
+def registration_view(request):
+    if request.method == 'POST':
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            login(request, User)
+            login(request, user)
+            return redirect('home')
     else:
         form = RegistrationForm()
 
-    return render(request, "auth_system/registration.html", {"form": form})
+    return render(request, 'auth_system/registration.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'auth_system/login.html', {'form': form})
+
+@login_required
+def logout_view(request):
+        logout(request)
+        return redirect('home')
+
+def home_view(request):
+    return render (request ,'home.html')
